580935dad34c0a2420a4c94c67a9ed2390145847
Maitree-5 <maitriyadav510@gmail.com>
2024-06-27 20:09:15 -0400

    quantexa

--- src/main/scala/Q3.scala
+++ src/main/scala/Q3.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, sum, expr, max}
+
 /**
  * Object Q3 contains methods for analyzing flight data to determine the longest non-UK travel streaks per passenger.
  */
@@ -14,18 +15,20 @@ object Q3 {
   def longestNonUKCount(flightDataDS: Dataset[FlightData]) = {
     // Add a new column 'ukArrivals' to indicate if the flight is arriving from the UK
     val ukFlights = flightDataDS.withColumn("ukArrivals",
-      sum( expr("case when from = 'uk' then 1 else 0 end"))
-        .over( Window.partitionBy(col("passengerId")).orderBy(col("date"))))
+      sum(expr("case when from = 'uk' then 1 else 0 end"))
+        .over(Window.partitionBy(col("passengerId")).orderBy(col("date"))))
+
     // Group by 'passengerId' and 'ukArrivals' and calculate the non-UK flight count
-    ukFlights.groupBy( col("passengerId" ), col("ukArrivals"))
-      .agg( (sum( expr(
+    val nonUKCounts = ukFlights.groupBy(col("passengerId"), col("ukArrivals"))
+      .agg(sum(expr(
         """case
           | when 'uk' not in (from,to) then 1
-          | when from = to then -1
           | else 0
-          |end""".stripMargin)) + 1).as("nonuk"))
-      .groupBy("passengerId")
-      .agg( max("nonuk").as("LongestRun"))
+          |end""".stripMargin)).as("nonuk"))
+
+    // Calculate the longest streak of non-UK flights for each passenger
+    nonUKCounts.groupBy("passengerId")
+      .agg(max("nonuk").as("LongestRun"))
       .orderBy(col("LongestRun").desc)
   }
 }
